9a030d64808d31bc9a84f3498ae14a7e254fb1b7
nhsmith85 <smith.nathanh@gmail.com>
2024-11-30 14:54:23 -0500

    eval

--- system/eval.py
+++ system/eval.py
@@ -15,26 +15,7 @@
 # execute the generated unit tests on the reference repository
 # compute the coverage of the generated unit tests has on the reference repository
 
-
-# for each directory in "system/output/v2"
-# cd into the directory
-# create a new conda environment (or virtualenv if easier?) from the requirements.txt
-# execute the repository's example code in each `examples` directory
-
-# import os
-
-# for directory in os.listdir("system/output/v2"):
-#     if os.path.isdir(directory):
-#         os.chdir(directory)
-#         os.system("conda create --name test_env --file requirements.txt")
-#         os.system("conda activate test_env")
-#         os.system("python examples/demo.py")
-#         os.system("pytest tests/unit/test_module.py")
-#         os.system("pytest tests/acceptance/test_features.py")
-#         os.system("coverage run -m pytest tests/unit/test_module.py")
-#         os.system("coverage report")
-#         os.system("conda deactivate")
-#         os.chdir("..")
+# --------------------------------------------------------------------------------------------
 
 # for each of the directories in "system/output/v3"
 # cd into the directory
